9eba310cb65efd8515cd3a053297a2f24f690f39
appchagio <appchagio@gmail.com>
2026-08-11 22:31:24 +0700

    Update OrderingView.tsx

--- src/components/OrderingView.tsx
+++ src/components/OrderingView.tsx
@@ -654,7 +654,7 @@ export const OrderingView: React.FC<OrderingViewProps> = ({
                       <td className="border-r-2 border-black p-1 text-left font-black text-[0.85em] uppercase tracking-tight font-sans leading-tight">
                         {removeVietnameseAccents(item?.name || 'MON').toUpperCase()}
                       </td>
-                      <td className="border-r-2 border-black p-1 text-center font-black text-[0.9em]">
+                      <td className="border-r-2 border-black p-1 text-center font-black text-[1.25em] leading-none">
                         {item?.quantity || 1}
                       </td>
                       <td className="p-1 text-right font-black text-[0.9em]">
