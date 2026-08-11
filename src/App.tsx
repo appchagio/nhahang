1324a970d661e2edb831a305c189dd54849b2f4c
appchagio <appchagio@gmail.com>
2026-08-11 13:48:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -296,6 +296,7 @@ export default function App() {
             orders={cachedOrders}
             menu={menu}
             printSettings={printSettings}
+            onUpdateRevenueRecords={(newRecords) => setRevenueRecords(newRecords)}
           />
         )}
 
